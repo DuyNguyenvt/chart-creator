cc94c1982d283bccd0c5b154dde9a40854a34b38
DuyNguyenvt <nnqduy.spkt@gmail.com>
2020-04-16 23:51:04 +0700

    test change username

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -5,14 +5,12 @@ import {
   Switch,
   BrowserRouter as Router,
 } from "react-router-dom";
-// import logo from "components/Icons/ReactLogo/logo.svg";
 import "themes/css/App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import TestSeedScreen from "containers/App/screens/TestSeedScreen/Injectable";
 import MyProfile from "containers/App/screens/MyProfile";
 import history from "utils/history";
 import Home from "containers/Home/Injectable";
-// import config from "global-config";
 
 function App() {
   console.log("history", history);
