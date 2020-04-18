dc8bf653f6825bd90940dd2b1a295fc8350323ad
DuyNguyenvt <nnqduy.spkt@gmail.com>
2020-04-18 16:10:12 +0700

    Updates

---
+++ precache-manifest.c2aa2c55ec7796374e6ecdf0c602628a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517beeda97c798b3b1dff7020003194b",
+    "url": "/chart-creator/index.html"
+  },
+  {
+    "revision": "1a8702d1e37dd92d2746",
+    "url": "/chart-creator/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "990ae57832eb623b6e89",
+    "url": "/chart-creator/static/css/main.ec9f28a0.chunk.css"
+  },
+  {
+    "revision": "1a8702d1e37dd92d2746",
+    "url": "/chart-creator/static/js/2.3ed2ddd3.chunk.js"
+  },
+  {
+    "revision": "77c9fe09814b52da3a9104c128191c21",
+    "url": "/chart-creator/static/js/2.3ed2ddd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990ae57832eb623b6e89",
+    "url": "/chart-creator/static/js/main.b96f6cad.chunk.js"
+  },
+  {
+    "revision": "c2178124b15dc2f5195b",
+    "url": "/chart-creator/static/js/runtime-main.ab90bd8d.js"
+  },
+  {
+    "revision": "47006ed70b4ef68411114aba3b638558",
+    "url": "/chart-creator/static/media/my-pic.47006ed7.jpg"
+  }
+]);
